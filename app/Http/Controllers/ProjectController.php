768f312a71f06bde43e829d805e4aff289e96ffa
Muhammad Hur <muhammedhur@gmail.com>
2023-10-08 22:14:23 +0500

    general changes

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -44,13 +44,31 @@ public function edit(Project $project)
 
 public function update(Request $request, Project $project)
 {
-    // Similar validation as store but considering unique constraints for update
-    // Then, update the project with validated data.
+    $validatedData = $request->validate([
+        'title' => 'required|string|min:5|unique:projects,title,' . $project->id . ',id,trimester,' . $request->trimester . ',year,' . $request->year,
+        'inp_name' => 'required|string|min:5',
+        'inp_email' => 'required|string|email',
+        'description' => 'required|regex:/^(\w+\s?){3,}$/',
+        'students_needed' => 'required|integer|min:3|max:6',
+        'trimester' => 'required|integer|min:1|max:3',
+        'year' => 'required|integer',
+    ]);
+
+    $project->update($validatedData);
+
+    return redirect()->route('projects.index')->with('message', 'Project updated successfully!');
 }
 
 public function destroy(Project $project)
 {
-    // Check for student applications. If none, delete, else redirect with an error.
+    // Assuming you have a related model for student applications named "Application"
+    // $applications = $project->applications;
+    // if (!$applications->isEmpty()) {
+    //     return redirect()->route('projects.index')->with('error', 'Cannot delete project with existing student applications.');
+    // }
+
+    $project->delete();
+
+    return redirect()->route('projects.index')->with('message', 'Project deleted successfully!');
 }
-    //
 }
--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -1,19 +1,63 @@
 <x-app>
 
+    <h1 class="text-2xl font-bold mb-6">Edit Project: {{ $project->title }}</h1>
 
-<h1>Edit Project: {{ $project->title }}</h1>
+    <form action="{{ route('projects.update', $project) }}" method="POST" class="space-y-4">
+        @csrf
+        @method('PUT')
 
-<form action="{{ route('projects.update', $project) }}" method="POST">
-    @csrf
-    @method('PUT')
+        <div class="flex flex-col space-y-2">
+            <label for="title" class="block mb-2">Title:</label>
+            <input type="text" name="title" value="{{ old('title', $project->title) }}" class="border rounded w-full py-2 px-3">
+            @error('title') <span class="text-red-500">{{ $message }}</span> @enderror
+        </div>
 
-    <!-- The form fields here will be similar to the create view, but with value attributes set to the current project's attributes. For example: -->
-    <input type="text" name="title" value="{{ old('title', $project->title) }}">
+        <div class="flex flex-col space-y-2">
+            <label for="inp_name" class="block mb-2">InP Name:</label>
+            <input type="text" name="inp_name" value="{{ old('inp_name', $project->inp_name) }}" class="border rounded w-full py-2 px-3">
+            @error('inp_name') <span class="text-red-500">{{ $message }}</span> @enderror
+        </div>
 
-    <!-- ... rest of the form fields ... -->
+        <div class="flex flex-col space-y-2">
+            <label for="inp_email" class="block mb-2">InP Email:</label>
+            <input type="email" name="inp_email" value="{{ old('inp_email', $project->inp_email) }}" class="border rounded w-full py-2 px-3">
+            @error('inp_email') <span class="text-red-500">{{ $message }}</span> @enderror
+        </div>
 
-    <input type="submit" value="Update Project">
-</form>
+        <div class="flex flex-col space-y-2">
+            <label for="description" class="block mb-2">Description:</label>
+            <textarea name="description" class="border rounded w-full py-2 px-3">{{ old('description', $project->description) }}</textarea>
+            @error('description') <span class="text-red-500">{{ $message }}</span> @enderror
+        </div>
 
+        <div class="flex flex-col space-y-2">
+            <label for="students_needed" class="block mb-2">Number of Students:</label>
+            <input type="number" name="students_needed" min="3" max="6" value="{{ old('students_needed', $project->students_needed) }}" class="border rounded w-full py-2 px-3">
+            @error('students_needed') <span class="text-red-500">{{ $message }}</span> @enderror
+        </div>
+
+        <div class="flex flex-col space-y-2">
+            <label for="trimester" class="block mb-2">Trimester:</label>
+            <select name="trimester" class="border rounded w-full py-2 px-3">
+                @for ($i = 1; $i <= 3; $i++)
+                    <option value="{{ $i }}" @if(old('trimester', $project->trimester) == $i) selected @endif>{{ $i }}</option>
+                @endfor
+            </select>
+            @error('trimester') <span class="text-red-500">{{ $message }}</span> @enderror
+        </div>
+
+        <div class="flex flex-col space-y-2">
+            <label for="year" class="block mb-2">Year:</label>
+            <input type="number" name="year" value="{{ old('year', $project->year) }}" class="border rounded w-full py-2 px-3">
+            @error('year') <span class="text-red-500">{{ $message }}</span> @enderror
+        </div>
+
+        <div class="flex items-center justify-between">
+        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-blue font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            Update Project
+        </button>
+    </div>
+
+    </form>
 
 </x-app>
